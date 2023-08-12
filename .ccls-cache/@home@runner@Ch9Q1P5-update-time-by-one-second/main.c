5d523c629c959b31548a1dfc4b34a5af64d65f7f
Iain Taylor <iain.taylor.94@gmail.com>
2023-08-12 14:08:50 +0000

    completed program

--- .ccls-cache/@home@runner@Ch9Q1P5-update-time-by-one-second/main.c
+++ .ccls-cache/@home@runner@Ch9Q1P5-update-time-by-one-second/main.c
@@ -1,6 +1,49 @@
+// Program to update the time by one second
+
 #include <stdio.h>
 
+// Define Structures
+struct time {
+int hour;
+int minutes; 
+int seconds;
+};
+
+// Declare Functions
+struct time timeUpdate (struct time);
+
 int main(void) {
-  printf("Hello World\n");
+
+  struct time currentTime, nextTime;
+
+  // Get input
+  printf ("Enter the time (hh:mm:ss): ");
+  scanf ("%d:%d:%d", &currentTime.hour, &currentTime.minutes, &currentTime.seconds);
+
+  // Calculate the next time
+  nextTime = timeUpdate (currentTime);
+
+  // Print output
+  printf ("Updated time is %.2d:%.2d:%.2d\n", nextTime.hour, nextTime.minutes, nextTime.seconds);
+  
   return 0;
+}
+
+// Define Functions
+struct time timeUpdate (struct time now) {
+  now.seconds++;
+  if (now.seconds == 60) { // Next minute
+    now.seconds = 0;
+    now.minutes++;
+  }
+  if (now.minutes == 60) { // Next hour
+    now.minutes = 0;
+    now.hour++;
+
+    if (now.hour == 24) { // Midnight
+      now.hour = 0;
+    }
+  }
+
+  return now;
 }
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Programming in C
+## Chapter 9
+### Question 1: Part 5
+
+This problem is from chapter 9 of the textbook[^1]. It is to copy Program 9.5[^2] from the textbook. This program prints tomorrows date.
+
+You may wish to run the program and compare its output.
+
+
+[^1]: Programming in C (Third edition) By Stephen G. Kochan - Page 191 - Question 1
+[^2]: Programming in C (Third edition) By Stephen G. Kochan - Page 178
